9c3327de2410332c97a333f8bfe261d655859f16
pratimacd98 <131126911+pratimacd98@users.noreply.github.com>
2026-04-19 15:23:37 +0530

    Update remediation.sh

--- scripts/remediation.sh
+++ scripts/remediation.sh
@@ -1,1 +1,13 @@
+# Command to disable public access on the insecure storage account
+az storage account update --name insecurestorage1 --resource-group RG-SC-Proj1 --allow-blob-public-access false
 
+
+
+# 1. Disable Public Network Access
+az storage account update --name insecurestorage1 --resource-group RG-SC-Proj1 --public-network-access Disabled
+
+# 2. Disable Shared Key Access
+az storage account update --name insecurestorage1 --resource-group RG-SC-Proj1 --allow-shared-key-access false
+
+# 3. Require TLS 1.2
+az storage account update --name insecurestorage1 --resource-group RG-SC-Proj1 --min-tls-version TLS1_2
